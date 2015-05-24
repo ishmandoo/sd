259d37c550425ecf0d1bc5bafca6a20366f8ca5f
philzook58 <philip@FartMachine7.local>
2015-05-24 18:44:17 -0400

    billable students route added

--- common/models/log.js
+++ common/models/log.js
@@ -1,5 +1,54 @@
 module.exports = function(Log) {
 
+
+  Log.billableStudents = function(date,cb){
+    date = new Date(date)
+    date.setDate(0)
+    date.setHours(0,0,0)
+    beginMonth = new Date(date)
+    date.setMonth(date.getMonth()+1)
+    endMonth = new Date(date)
+
+    Log.find(
+      {where:
+        {and:[
+          {date:{gt:beginMonth}},
+          {date:{lt:endMonth}}
+         ]},
+        order: 'studentId DESC'}, function(err,logs){
+          if(err) cb(err)
+          else{
+            if(logs){
+              if(logs.length > 0){
+              count = 1
+
+              for(var i = 1; i < logs.length; i++){
+                if(logs[i-1].studentId.toString() != logs[i].studentId.toString()){
+                  count = count + 1
+                }
+              }
+                cb(null,count)
+
+              }else{
+                cb(null,0)
+              }
+            }else{
+              cb(null,0)
+            }
+          }
+     })
+  }
+
+  Log.remoteMethod('billableStudents',{
+    accepts: {arg: 'date', type: 'date'},
+    returns: {arg: 'billableStudents', type: 'number'},
+    http: {path: '/billablestudents', verb: 'get'}
+  });
+
+
+
+
+
   Log.observe('before save', function(ctx, next) {
 
     ctx.instance.eventName = getEventName(ctx.instance.event);
