ecd13d1394dec9a9dac1219b0178c0c2e9a05b1e
Ben <ishmandoo@gmail.com>
2015-05-27 21:59:43 -0400

    added class import

--- client/js/import_controller.js
+++ client/js/import_controller.js
@@ -9,17 +9,21 @@
 
     function importController($http) {
         var vm = this;
-        vm.list = "";
+        vm.addStudentList = "";
+        vm.addStudentToClassList = "";
         vm.importStudents = importStudents;
+        vm.addStudentsToClass = addStudentsToClass;
+        vm.classList = [];
+        vm.selectedClass = "test";
 
         activate();
 
         function activate() {
-
+          getClasses();
         }
 
         function importStudents() {
-          var list = vm.list.split('\n');
+          var list = vm.addStudentList.split('\n');
           for (var i = 0; i<list.length; i++) {
 
             (function(name) {
@@ -33,13 +37,65 @@
                   $http.post('/api/students/', {name:name, status:"checked out", last_action_date:new Date()})
                   .success(function(student){
                   });
+                } else {
+                  vm.addStudentList += (name + "\n");
                 }
               });
             })(list[i])
 
           }
 
-          vm.list="";
+          vm.addStudentList="";
+        }
+
+        function addStudentsToClass() {
+          var list = vm.addStudentToClassList.split('\n');
+          for (var i = 0; i<list.length; i++) {
+
+            (function(name) {
+              $http({
+                url: '/api/students',
+                method: "GET",
+                params: {filter: {where:{name:name}}}
+              }).success(function(data){
+                if (data.length == 1) {
+                  addStudent(data[0].id);
+                } else {
+                  vm.addStudentsToClassList += (name + "\n");
+                }
+              });
+            })(list[i])
+
+          }
+
+          vm.addStudentToClassList="";
+        }
+
+        function addStudent(studentId){
+          $http.put('/api/classes/'+vm.selectedClass.id+'/students/rel/'+studentId,{
+            checked_in:false,
+            days_of_week:{
+              monday:true,
+              tuesday:true,
+              wednesday:true,
+              thursday:true,
+              friday:true,
+              saturday:false,
+              sunday:false
+            }
+          })
+          .success(function(student){
+            console.log(student)
+          });
+        }
+
+        function getClasses() {
+          $http({
+            url: '/api/classes',
+            method: "GET"
+          }).success(function(data){
+            vm.classList = data;
+          });
         }
     }
 })();
